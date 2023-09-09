c7f040dd77a5ba1ffea37893f8693670553ad489
ParitaChothani <133861647+ParitaChothani@users.noreply.github.com>
2023-09-09 14:54:55 +0530

    Module 6 Part 1

---
+++ all_assignment/Question15.java
@@ -0,0 +1,34 @@
+package all_assignment;
+
+public class Question15 {
+
+	public static void main(String[] args) {
+		
+		/*
+	     * 
+	     * 15. max. or min. value
+	     * 
+	     */
+		
+		
+		int[] arr= {3,5,9,7,6};
+		int min=arr[0];
+		int max=arr[0];
+		
+		for (int i =0; i < arr.length; i++) {
+			if(arr[i]<min) {
+				min=arr[i];
+			}
+			else if (arr[i]>max) {
+				max=arr[i];
+				
+			}
+		}
+		
+		System.out.println(" Minimum Value Of Array : " +min);
+		
+		System.out.println(" Maximum Value Of Array : " +max);
+	}	
+
+	}
+
